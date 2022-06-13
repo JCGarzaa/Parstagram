b054d603f43740114dc4da0b818da2f781c661db
JC Garza <jcgarza@fb.com>
2022-06-13 16:30:47 -0700

    Implemented Pull to Refresh Feed

--- app/src/main/java/com/example/parstagram/FeedActivity.java
+++ app/src/main/java/com/example/parstagram/FeedActivity.java
@@ -3,6 +3,7 @@ package com.example.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,7 @@ public class FeedActivity extends AppCompatActivity {
     public static final String TAG = "FeedActivity";
 
     private RecyclerView rvPosts;
+    private SwipeRefreshLayout swipeContainer;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
 
@@ -35,6 +37,24 @@ public class FeedActivity extends AppCompatActivity {
 
         rvPosts.setAdapter(adapter);
         rvPosts.setLayoutManager(new LinearLayoutManager(this));
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                allPosts.clear();           // clear data
+                queryPosts();               // fetch data
+                swipeContainer.setRefreshing(false);        // hide refresh icon
+
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
         queryPosts();
     }
 
