e8d127d02c0d7832cda51d7ab29c71cb562fdcd7
JC Garza <jcgarza@fb.com>
2022-06-13 15:35:52 -0700

    Initial commit - Completed Parse database setup, login, logout, posting photos using camera and description to database.

---
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("MqYbOKVPmiY5UEaOmVqSgN8KBrP3wHxEWM6pcsMT")
+                .clientKey("BF81FOGu8m1J56E6dixATWzXo05W4rA4x7X1sZGq")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
